2787042fdf48de900ad94c82ea28ac999a07954f
Gudbrand <gudbrandwd@live.no>
2024-01-10 10:39:35 +0100

    core done

--- csharp-fundamentals-control-flow.Main/Program.cs
+++ csharp-fundamentals-control-flow.Main/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_fundamentals_control_flow.Main;
+
+Core core = new Core();
+Extension extension = new Extension();
